04647f6a30c06978e187e28aa41bc52899ddb071
Jacob-Jeffries <jeffrij85@gmail.com>
2022-11-13 16:29:19 -0500

    created js file and added an if condition

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,19 @@
 var shape = "circle";
 var topics ="HTML, CSS, Git, JavaScript";
 console.log(topics);
+
+var topic = "HTML";
+
+//Here the topic variable is a string so we are comparing it to a string in the if statement, no type coercion is needed so we use ===
+
+if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+  } else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+  } else if (topic === 'Git') {
+    console.log("Let's study Git!");
+  } else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+  } else {
+    console.log('Please try again!');
+  }
